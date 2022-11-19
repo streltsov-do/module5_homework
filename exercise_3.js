b6206e7529671f7ceabebaced8b62adab37a1ee5
Denis <streltsov.do@gmail.com>
2022-11-19 19:39:49 +0300

    m5_homework: +desc; e3 - вариант через split("").reverse().join("");

--- exercise_3.js
+++ exercise_3.js
@@ -1,3 +1,10 @@
+/*
+    Дана строка. 
+    Необходимо вывести в консоль перевёрнутый вариант. 
+    Например, "Hello" -> "olleH".
+*/
+
+
 let inputStr=prompt("Введите строку");
 console.log(`Входная строка = ${inputStr}`);
 console.log(`Длина входной строки = ${inputStr.length}`);
@@ -8,3 +15,39 @@ for (let i=0; i<inputStr.length; i++){
     outputStr = outputStr + inputStr.slice(inputStr.length-i-1,inputStr.length-i);
 }
 console.log(`Выходная строка = ${outputStr}`);
+
+////////////////////////////////////
+// с помощью split + reverce + join
+let inputStr1=prompt("Введите строку");
+console.log(`Входная строка = ${inputStr1}`);
+console.log(`Длина входной строки = ${inputStr1.length}`);
+
+console.log(`--------------------------------------------------`);
+let outStr1_1=inputStr1.split("");
+console.log(`Тип строки split("") = ${typeof(outStr1)}`);
+console.log(`Строка после split("") - массив? answer=${Array.isArray(outStr1_1)}`);
+console.log(`Выходная строка + split("") = ${outStr1_1}`);
+
+let outStr1_2=inputStr1.split("").reverse();
+console.log(`Выходная строка + split("") + reverse = ${outStr1_2}`);
+
+let outStr1_3=inputStr1.split("").reverse().join("");
+console.log(`Выходная строка + split("") + reverse + join("") = ${outStr1_3}`);
+
+let outStr1_4=inputStr1.split("").reverse().join();
+console.log(`Тип строки split("").reverse().join() = ${typeof(outStr1_4)}`);
+console.log(`Строка после split("").reverse().join() - массив? answer=${Array.isArray(outStr1_4)}`);
+console.log(`Выходная строка + split("").reverse().join() = ${outStr1_4}`);
+
+console.log(`--------------------------------------------------`);
+console.log(`Некорректный вариант`)ж
+let outStr2_1=inputStr1.split();
+console.log(`Тип строки split() = ${typeof(outStr2_1)}`);
+console.log(`Строка после split() - массив? answer=${Array.isArray(outStr2_1)}`);
+console.log(`Выходная строка + split() = ${outStr2_1}`);
+
+let outStr2_2=inputStr1.split().reverse();
+console.log(`Выходная строка + split().reverse() = ${outStr2_2}`);
+
+let outStr2_3=inputStr1.split().reverse().join();
+console.log(`Выходная строка + split().reverse().join() = ${outStr2_3}`);
--- exercise_4.js
+++ exercise_4.js
@@ -1,2 +1,7 @@
+/* 
+    Записать в переменную случайное целое число в диапазоне [0; 100]. 
+    Используйте объект Math.
+*/
+
 let randomNum = Math.round((Math.random() * 100));
 console.log(randomNum);
